1772c907a1c50396c64a3597bc83a621872eb07e
Narimene Lamini <lamininarimene123@gmail.com>
2022-07-29 09:32:24 +0100

    Editing Todo component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,10 @@
 import React , {useState} from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo';
 
 function TodoList() {
 const [todos, setTodos] = useState([]);
-
+//addTodo function
 const addTodo = todo =>{
     if(!todo.text || /^\s*$/.test(todo.text)){
         return;
@@ -12,12 +13,40 @@ const addTodo = todo =>{
     setTodos(newTodos);
     console.log(...todos);
 }
+//updateTodo function
+const updateTodo = (todoId, newValue) =>{
+    if(!newValue.text || /^\s*$/.test(newValue.text)){
+        return;
+    }
+    setTodos(prev => prev.map(item => (item.id  === todoId? newValue : item )))
+}
+//completeTodo function
+ const completeTodo = id => {
+     let updatedTodos = todos.map(todo => {
+        if (todo.id === id ){
+            todo.isComplete = !todo.isComplete;
+        }
+        return todo
+     })
+     setTodos(updatedTodos);
+ }
+ //removeTodo function
+ const removeTodo = id =>{
+    const removeArr = [...todos].filter(todo =>  todo.id !== id);
+    setTodos(removeArr);
+ }
+
 
 
   return (
     <div>
         <h1>What is your plan for today ?</h1>
         <TodoForm onSubmit= {addTodo} />
+        <Todo 
+        todos = {todos} 
+        completeTodo = {completeTodo} 
+        updateTodo = {updateTodo}
+        removeTodo = {removeTodo} />
     </div>
   )
 }
